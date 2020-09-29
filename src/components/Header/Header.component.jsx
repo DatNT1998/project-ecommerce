9685835f47ba0c8f8c90335513d0fb5aee57b49f
DatNT1998 <44127716+DatNT1998@users.noreply.github.com>
2020-09-29 17:37:50 +0700

    header component

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
-import './Header.styles.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class Header extends Component {
-    render() {
-        return (
-            <div>
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-            </div>
-        );
-    }
-}
+import './header.styles.scss';
+
+const Header = () => (
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOP
+      </Link>
+            <Link className='option' to='/shop'>
+                CONTACT
+      </Link>
+        </div>
+    </div>
+);
 
 export default Header;
